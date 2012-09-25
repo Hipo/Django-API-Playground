7e7060c3ed2f6e198c6d4ed614e00baff384fb65
Fatih Erikli <fatiherikli@gmail.com>
2012-09-26 01:52:13 +0300

    updated example local settings

--- example/example/settings_local.py.ex
+++ example/example/settings_local.py.ex
@@ -1,7 +1,7 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'data',
+        'NAME': 'test-data',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
