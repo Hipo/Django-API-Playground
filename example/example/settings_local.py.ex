644b4b34636cd04a093623596fd2f3ebd6746fe6
Fatih Erikli <fatiherikli@gmail.com>
2012-09-26 01:18:56 +0300

    added settings_local example

---
+++ example/example/settings_local.py.ex
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'data',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
+SECRET_KEY = 'generate-a-secret-key'
