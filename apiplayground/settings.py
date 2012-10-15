d7c61023c35bd81bad0f92bc4bc69a374a547148
Areski Belaid <areski@gmail.com>
2012-10-15 12:15:29 +0200

    fix settings API_PLAYGROUND_FEEDBACK

--- apiplayground/settings.py
+++ apiplayground/settings.py
@@ -4,4 +4,5 @@ API_PLAYGROUND_SCHEMA_PATH = getattr(settings,
     "API_PLAYGROUND_SCHEMA_PATH", "api_playground_schema.json")
 
 # Toggle the feedback form
-API_PLAYGROUND_FEEDBACK = True
+API_PLAYGROUND_FEEDBACK = getattr(settings,
+    "API_PLAYGROUND_FEEDBACK", True)
