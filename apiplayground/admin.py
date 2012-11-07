f5a54ee3d7e78a3c01d0b8e13465a1a507cbb930
Drew Engelson <drew@engelson.net>
2012-11-07 00:15:39 -0500

    Fixed tab issue.

--- apiplayground/admin.py
+++ apiplayground/admin.py
@@ -3,7 +3,7 @@ from apiplayground.models import Feedback
 
 
 class FeedbackAdmin(admin.ModelAdmin):
-	"""Configures admin for Feedback model."""
+    """Configures admin for Feedback model."""
     list_display = ('title', 'resource', 'status', 'date_created', )
     list_filter = ('status', )
     ordering = ('-date_created', )
